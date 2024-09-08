04b6aa21d02ea194e0e838a2dc671c3d3bc272af
J-c0d3r <juan.ferrugini@ges.inatel.br>
2024-09-08 12:04:52 -0300

    revision karate yt - done

---
+++ KarateAPI_YT/src/test/java/yt_test/LearningConfigFile.feature
@@ -0,0 +1,14 @@
+Feature: Learning about karate-config.js
+
+  Background: 
+    * url baseUrl
+    * header Accept = 'application/json'
+
+  Scenario: Printing Global Var
+    Given print myVarName
+
+  Scenario: Method GET using ConfigFile resource
+    Given path '/users?page=2'
+    When method GET
+    Then status 200
+    And print response
--- KarateAPI_YT/src/test/java/yt_test/YTTestRunner.java
+++ KarateAPI_YT/src/test/java/yt_test/YTTestRunner.java
@@ -1,13 +1,19 @@
 package yt_test;
 
 import com.intuit.karate.junit5.Karate;
+import org.junit.BeforeClass;
 
 class YTTestRunner {
 
+    @BeforeClass
+    public static void before() {
+        System.setProperty("karate.env", "qa");
+    }
+
     @Karate.Test
     Karate runYTTest() {
-        /* return Karate.run("GET", "POST").relativeTo(getClass()); */
-        return Karate.run("DELETE").relativeTo(getClass());
+        return Karate.run().relativeTo(getClass());
+        /* return Karate.run("PUT").relativeTo(getClass()); */
     }
 
 }
